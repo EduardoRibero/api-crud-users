d2d5e19399085c7f44b7e4be7b64c22bd6119f63
EduardoRibero <ribeirodomingues2020@gmail.com>
2024-07-01 11:32:10 -0300

    Config CORS

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ public class UsersController {
 	@Autowired
 	private UsersService usersService;
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping
 	public List <UsersDTO> findAll(){
 		List <UsersDTO> result = usersService.findAll();
@@ -35,18 +37,21 @@ public class UsersController {
 		return result;
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping
 	public UsersDTO postUsers(@RequestBody Users users) {
 		UsersDTO dto = usersService.salveUsers(users);
 		return dto;
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@DeleteMapping(value = "/{id}")
 	public boolean deleteUsers(@PathVariable long id) {
 			usersService.deleteUsers(id);
 			return true;
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PutMapping(value = "/{id}")
 	public UsersDTO update(@PathVariable long id, @RequestBody Users users) {
 		return usersService.updateUser(id, users);
